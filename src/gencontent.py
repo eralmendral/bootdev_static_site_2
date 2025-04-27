f3cd935609655cccf94b7bb3312e869f0400d99a
Eric Almendral <eralmndrx@gmail.com>
2025-04-27 15:30:56 +0800

    Generate page recursively

--- src/gencontent.py
+++ src/gencontent.py
@@ -37,3 +37,13 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for root, _, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                relative_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dest_dir_path, os.path.splitext(relative_path)[0] + ".html")
+                generate_page(from_path, template_path, dest_path)
